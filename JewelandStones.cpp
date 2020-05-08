ff864da69b1c9d043711941a633da311479df540
Ashwathy Anil Alappatt <ashwathyanil.mec@gmail.com>
2020-05-08 19:33:39 +0530

    first commit

---
+++ JewelandStones.cpp
@@ -0,0 +1,24 @@
+#You're given strings J representing the types of stones that are jewels, and S #representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+#The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+#include<map>
+#include<iostream>
+class Solution {
+    map<char,int>nmap;
+public:
+    int numJewelsInStones(string J, string S) {
+        int count = 0;
+        for(auto x:J)
+        {
+            nmap[x] = 1;
+        }
+        
+        for(auto x:S)
+        {
+            if(nmap[x] == 1)
+                count++;
+        }
+        return count;
+    }
+};
